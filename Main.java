5d7577eeff0a3aece28d04b82ca9e3bfb777ab60
Kurkomisu <114882650+Kurkomisu@users.noreply.github.com>
2024-02-18 01:06:01 +0100

    Added function to read cities from file.

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class Main {
+    public static void main(String []args){
+        //System.out.println("Hello World");
+
+            CitiesReader("cities");
+
+
+    }
+    public static void CitiesReader(String filename) {
+        try(FileReader fr = new FileReader(filename + ".txt")) {
+            int i = 0;
+            while((i = fr.read()) != -1){
+
+                System.out.print((char)i);
+            }
+        }
+        catch (IOException e){
+            System.out.print(e.getMessage());
+        }
+
+
+    }
+}
